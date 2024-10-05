0a6a2e9571867a54e299e75350ce310590338e68
OIer Kimi <15381388023@163.com>
2024-10-05 06:13:24 +0000

    Backup on 2024-10-05T05-58

---
+++ 2.sh
@@ -0,0 +1,15 @@
+
+rm -rf './backup'
+mkdir -p './backup'
+hydrooj backup
+OUTPUT=$(ls -t | grep '^backup-' | head -n 1)
+
+if [[ -z "$OUTPUT" ]]; then
+    echo '未找到备份文件'
+    exit 1
+fi
+
+echo "找到备份文件: $OUTPUT"  # 输出 OUTPUT 的值
+split -b 10M $OUTPUT "./backup/backup-part"
+rm $OUTPUT
+
---
+++ 3.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m 'Backup on 2024-10-05T05-58'
+git push
